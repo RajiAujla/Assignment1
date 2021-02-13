3b858038a4efd6479cfa9f881a78dc65148252ec
RajiAujla <Rajwinderaujla061@gmail.com>
2021-02-12 21:58:05 -0500

    commit as containing the completed Rectangle class

--- Assignment1/Program.cs
+++ Assignment1/Program.cs
@@ -10,6 +10,15 @@ namespace Assignment1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Figure 1. Example Assignment 01 Solution");
+            Console.WriteLine("-----------------------------------------\n");
+            Console.WriteLine("Enter Length of rectangle:");
+            int userLength = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Enter Width of rectangle");
+            int userWidth = Convert.ToInt16(Console.ReadLine());
+
+            RectangleClass obj = new RectangleClass(userLength, userWidth);
+
         }
     }
 }
---
+++ Assignment1/RectangleClass.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1
+{
+    public class RectangleClass
+    {
+        private int length_rec, width_rec;
+
+        //Default Constructor
+        public RectangleClass()
+        {
+            length_rec = 1;
+            width_rec = 1;
+        }
+
+        //Non-default constructor
+        public RectangleClass(int userLength,int userWidth)
+        {
+            length_rec = userLength;
+            width_rec = userWidth;
+        }
+
+        //Methods
+        public int GetLength()
+        {
+            return length_rec;
+        }
+
+        public int SetLength(int length)
+        {
+           return length_rec = length;
+        }
+
+        public int GetWidth()
+        {
+            return width_rec;
+        }
+
+        public int SetWidth(int width)
+        {
+            return width_rec = width;
+        }
+
+        public int GetPerimeter()
+        {
+            return 2*(length_rec + width_rec);
+        }
+
+        public int GetArea()
+        {
+            return length_rec * width_rec;
+        }
+    }
+}
